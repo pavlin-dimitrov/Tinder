daf6a94a2fdf5e7c75844132872e74901742886f
Pavlin Dimitrov <pavlin.k.dimitrov@gmail.com>
2022-12-28 12:31:19 +0200

    Adding Swagger documentation to Register User

--- src/main/java/com/volasoftware/tinder/DTO/AccountRegisterDTO.java
+++ src/main/java/com/volasoftware/tinder/DTO/AccountRegisterDTO.java
@@ -2,6 +2,8 @@ package com.volasoftware.tinder.DTO;
 
 import com.volasoftware.tinder.enums.Gender;
 import com.volasoftware.tinder.validator.password.ValidPassword;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,29 +16,35 @@ import javax.validation.constraints.*;
 @Getter
 @Setter
 @ToString
+@ApiModel(value = "Account model", description = "Model representing an account entity")
 public class AccountRegisterDTO {
+    @ApiModelProperty(value = "Account's first name", required = true)
     @Size(min = 2, max = 50)
     @Pattern(regexp = "^[A-Za-z-]*$")
     @NotBlank
     @NotNull
     private String firstName;
 
+    @ApiModelProperty(value = "Account's last name", required = true)
     @Size(min = 2, max = 50)
     @Pattern(regexp = "^[A-Za-z-]*$")
     @NotBlank
     @NotNull
     private String lastName;
 
+    @ApiModelProperty(value = "Account's email address", required = true)
     @NotBlank
     @NotNull
     @Email
     private String email;
 
+    @ApiModelProperty(value = "Account's password", required = true)
     @ValidPassword
     @NotBlank
     @NotNull
     private String password;
 
+    @ApiModelProperty(value = "Account's gender", required = true)
     @Enumerated(EnumType.STRING)
     @NotNull
     private Gender gender;
--- src/main/java/com/volasoftware/tinder/controller/AccountController.java
+++ src/main/java/com/volasoftware/tinder/controller/AccountController.java
@@ -2,14 +2,17 @@ package com.volasoftware.tinder.controller;
 
 import com.volasoftware.tinder.DTO.AccountDTO;
 import com.volasoftware.tinder.DTO.AccountRegisterDTO;
-import com.volasoftware.tinder.entity.Account;
 import com.volasoftware.tinder.service.contract.AccountService;
-import java.util.List;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -18,12 +21,26 @@ public class AccountController {
 
   @Autowired private final AccountService accountService;
 
+  @ApiOperation(value = "Retrieves a list of accounts")
+  @ApiResponses(value = {
+          @ApiResponse(code = 200, message = "Successfully retrieved list of accounts"),
+          @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+          @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+          @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+  })
   @GetMapping
   public ResponseEntity<List<AccountDTO>> getAllAccounts() {
     List<AccountDTO> accounts = accountService.getAccounts();
     return ResponseEntity.ok(accounts);
   }
 
+  @ApiOperation(value = "Create new account / registration")
+  @ApiResponses(value = {
+          @ApiResponse(code = 200, message = "Successfully registered new account"),
+          @ApiResponse(code = 401, message = "Not authorized action"),
+          @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+          @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+  })
   @PostMapping("/register")
   public ResponseEntity<AccountRegisterDTO> createAccount(@RequestBody AccountRegisterDTO dto) {
     AccountRegisterDTO newAccount = accountService.addNewAccount(dto);
