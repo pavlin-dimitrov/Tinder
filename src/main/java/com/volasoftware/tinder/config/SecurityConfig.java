43c7acfd56b92d848c4465ff909c04107f808caf
Pavlin Dimitrov <pavlin.k.dimitrov@gmail.com>
2023-01-07 12:21:58 +0200

    Finish tests for verification tokens

--- src/main/java/com/volasoftware/tinder/config/SecurityConfig.java
+++ src/main/java/com/volasoftware/tinder/config/SecurityConfig.java
@@ -1,17 +1,45 @@
-//package com.volasoftware.tinder.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//
-//@Configuration
-//public class SecurityConfig extends AbstractHttpConfigurer<SecurityConfig, HttpSecurity> {
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/**").permitAll()
-//                .antMatchers("/**").permitAll();
-//    }
-//}
+package com.volasoftware.tinder.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    //TODO JwtAuthenticationFilter class.
+    // before sec.2920 - https://www.youtube.com/watch?v=KxqlJblhzfI&t=2920s
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers()
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
