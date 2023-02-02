0dc046bf03aca205205c1e4b14bdfc525f6e64d4
pavlin-dimitrov <pavlin.k.dimitrov@gmail.com>
2023-02-02 15:35:09 +0200

    Friends controller refactoring 2

--- src/main/java/com/volasoftware/tinder/controller/FriendsController.java
+++ src/main/java/com/volasoftware/tinder/controller/FriendsController.java
@@ -1,11 +1,15 @@
 package com.volasoftware.tinder.controller;
 
+import com.volasoftware.tinder.DTO.FriendDTO;
 import com.volasoftware.tinder.DTO.ResponseDTO;
 import com.volasoftware.tinder.service.contract.FriendsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,16 +32,25 @@ public class FriendsController {
   @ApiOperation(value = "Seed friends")
   @ApiResponses(
       value = {
-          @ApiResponse(code = 200, message = "Successfully seeded friends"),
-          @ApiResponse(code = 404, message = "The resource is not found")
+        @ApiResponse(code = 200, message = "Successfully seeded friends"),
+        @ApiResponse(code = 404, message = "The resource is not found")
       })
   @GetMapping("/seed-friends")
   public ResponseEntity<ResponseDTO> seedFriends(@RequestParam(required = false) Long accountId) {
     if (accountId == null) {
-      return new ResponseEntity<>(friendsService.linkingAllRealAccountsWithRandomFriends(), HttpStatus.OK);
+      return new ResponseEntity<>(
+          friendsService.linkingAllRealAccountsWithRandomFriends(), HttpStatus.OK);
     } else {
       return new ResponseEntity<>(
           friendsService.linkingRequestedRealAccountWithRandomFriends(accountId), HttpStatus.OK);
     }
+  }
+
+  @GetMapping("/friends")
+  public ResponseEntity<List<FriendDTO>> showListOfFriends(
+      @ApiParam(value = "The authenticated user", required = true) @RequestParam
+          Principal principal) {
+    return ResponseEntity<>(friendsService.showAllMyFriendsOrderedByClosestLocation(principal.getName()))
+
   }
 }
