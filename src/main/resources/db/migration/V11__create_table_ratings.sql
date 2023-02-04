24a0c9c6516973ebdd13d8376ad943f1b4dfe28f
pavlin-dimitrov <pavlin.k.dimitrov@gmail.com>
2023-02-04 13:30:43 +0200

    Add Ratings to database with Flyway migration

---
+++ src/main/resources/db/migration/V11__create_table_ratings.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ratings (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  account_id BIGINT NOT NULL,
+  friend_id BIGINT NOT NULL,
+  rating INT NOT NULL,
+  FOREIGN KEY (account_id) REFERENCES account(id),
+  FOREIGN KEY (friend_id) REFERENCES account(id),
+  CHECK (rating >= 1 AND rating <= 5)
+);
